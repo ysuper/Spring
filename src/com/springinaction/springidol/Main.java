1f1f2f6d6caa8db6671bbe892348fc9426113685
Unknown <ysuper0412@yahoo.com.tw>
2018-01-22 23:47:11 +0800

    use spring 4.3.9

--- src/com/springinaction/springidol/Main.java
+++ src/com/springinaction/springidol/Main.java
@@ -1,35 +1,31 @@
 package com.springinaction.springidol;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-
-import onlyfun.caterpillar.HelloBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Resource rs = new FileSystemResource("spring-idol.xml");
-		BeanFactory factory = new XmlBeanFactory(rs);
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");  
 		
 		// new com.springinaction.springidol.Juggler(); 
-		Performer performer1 = (Performer) factory.getBean("duke");
+		Performer performer1 = (Performer) ctx.getBean("duke");
 		performer1.perform();
 		
 		// Poem sonnet29 = new Sonnet29();  
 		// Performer duke = new PoeticJuggler(15, sonnet29); 
-		Performer performer2 = (Performer) factory.getBean("poeticDuke");
+		Performer performer2 = (Performer) ctx.getBean("poeticDuke");
 		performer2.perform();
 		
 //		Stage performer = new Stage();
-		Stage stage = (Stage) factory.getBean("theStage");
+		Stage stage = (Stage) ctx.getBean("theStage");
 //		performer3.getInstance();
 //		Stage.getInstance();
 		
-		Auditorium auditorium = (Auditorium) factory.getBean("auditorium");
+		Auditorium auditorium = (Auditorium) ctx.getBean("auditorium");
 		
-		Performer performer3 = (Performer) factory.getBean("kenny");
+		Performer performer3 = (Performer) ctx.getBean("kenny");
 		performer3.perform();
 		
 	}
